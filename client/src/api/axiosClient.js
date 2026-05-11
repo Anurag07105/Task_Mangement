57d2b045779579462cf4132c989656cd7b7842ed
Anurag07105 <aj418566@gmail.com>
2026-05-12 04:06:09 +0530

    f origin updated

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosClient = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
-    "https://teamtaskmanger-production-a7a3.up.railway.app/api",
+    "https://taskmangement-production-31da.up.railway.app/api",
 });
 
 axiosClient.interceptors.request.use((config) => {
